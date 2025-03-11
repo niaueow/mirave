7a17f947c9bed70ce1bf3cbb9b3641917411f455
nia <shabrinaniaa@gmail.com>
2025-03-11 09:48:13 +0700

    initialize React app with Vite and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="text-center">
+      <h1 className="text-2xl font-bold text-blue-200">Photobooth App</h1>
+    </div>
+  );
+}
+
+export default App
